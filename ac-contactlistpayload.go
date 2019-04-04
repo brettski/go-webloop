5ee0bdac9041c0037f67d0000404bb473a286141
Brett Slaski <brettski@yahoo.com>
2019-04-04 18:40:43 -0500

    Add struct for contact list json

---
+++ ac-contactlistpayload.go
@@ -0,0 +1,14 @@
+package main
+
+// Active Campain payload when filtering contacts by email
+
+// AcContactListPayload struct modeling payload received from AC filtered contact list
+type AcContactListPayload struct {
+	Contact []struct {
+		Email     string `json:"email"`
+		FirstName string `json:"firstName"`
+		LastName  string `json:"lastName"`
+		Id        string `json:"id"`
+		Org       string `json:"organization"`
+	}
+}
